fc7b1ac6f813f6d3b3261f07265e33ce518b9223
DC-t-spec <dcnet025@gmail.com>
2026-02-13 12:58:12 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
+const CACHE = "dc-one-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./script.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
 
