ded64663e3b69b447c174b735a260c52891f5a08
DC-t-spec <dcnet025@gmail.com>
2026-03-05 23:51:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,8 @@
     SUPABASE_ANON_KEY:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impub3Z3aWp4dXBscmNqYnRuZXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTI0MjcsImV4cCI6MjA4NjQ4ODQyN30.99oSvJ94iq4IfukLQTCFi5DvdAPbooDV4Ll9KgAuGD0",
 
+    console.log("✅ SUPABASE_URL (app):", DC_CONFIG.SUPABASE_URL);
+  
     AUTH_EMAIL_DOMAIN: "dc",
     PLANS: ["basic", "pro", "inteligente"],
 
